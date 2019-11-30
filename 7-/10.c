69e9f3b5d4f1925a77bbc9da9db7bffb380a895c
TonyTown6033 <55993162+TonyTown6033@users.noreply.github.com>
2019-11-30 16:56:43 +0800

    你

--- 7-/10.c
+++ 7-/10.c
@@ -3,17 +3,15 @@
 int main()
 {
   char str[20];
-  int tr=1,i=0,j=0;
+  int i=0,j=0;
   int length=0,u=0;
 
   printf("please input the string\n" );
   gets(str);
 
   while (i<=strlen(str)) {
-    if(tr==1)
       j++;
-    if(str[i]=='\0'){
-      tr=0;
+    if(str[i]==' '){
       if(length<=j){
         length=j;
         u=i-j;
@@ -23,7 +21,7 @@ int main()
     }
   }
 
-  while (str[u]!='\0') {
+  while (str[u]!=' ') {
     /* code */
     printf("%c",str[u] );
     u++;
