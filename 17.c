42a0fa4de72877a32eafcb994d285e7f52daf7e6
TonyTown6033 <55993162+TonyTown6033@users.noreply.github.com>
2019-12-08 19:30:04 +0800

    f

---
+++ 17.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+    int num ;
+    char transform(int );
+    char str[20];
+
+    printf("please input the int num\n" );
+    scanf("%d",&num );
+
+    str = transform(num);
+    puts(str);
+    return 0;
+
+}
+
+char transform(int n){
+  char str[20];
+
+
+}
