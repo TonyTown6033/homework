3a5fe4d45606c82cb597f4b675c9bcc064b00474
TonyTown6033 <55993162+TonyTown6033@users.noreply.github.com>
2019-12-26 10:44:07 +0800

    Update mzhere.c

--- mzhere.c
+++ mzhere.c
@@ -56,9 +56,6 @@ void arrange(void){
   int i,temp;
   for (i=0;i<p_num;i++)
     {c[i][0]=ave1[i];c[i][1]=i+1;}
-    for (i=0;i<p_num;i++){
-      printf("%f %f\n",c[i][0],c[i][1]);
-    }
   for(i=1;i<p_num;i++){
     if(c[i][0]>c[i-1][0])
     {temp=c[i][0];c[i][0]=c[i-1][0];c[i-1][0]=temp;
